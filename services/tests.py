a1042e6c90cfdbeedb6f8fd2abc56e795f4d3aa8
henry_23 <henry.maoht@outlook.com>
2021-12-24 22:03:56 +0800

    feat: add grouping function

--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+"""
+{
+    "projects": {
+        "proj_1_id": {
+            "max_group_num": 3
+        }, 
+        "proj_2_id": {
+            "max_group_num": 3
+        }, 
+        "proj_3_id": {
+            "max_group_num": 2
+        }, 
+        "proj_4_id": {
+            "max_group_num": 4
+        }
+    },
+    "wishes": [
+        {
+            "team_id": "team_id_1",
+            "choices": [
+                "proj_3_id", "proj_1_id", "proj_2_id"
+            ]
+        },
+        {
+            "team_id": "team_id_2",
+            "choices": [
+                "proj_1_id", "proj_3_id", "proj_2_id"
+            ]
+        },
+        {
+            "team_id": "team_id_3",
+            "choices": [
+                "proj_4_id", "proj_3_id", "proj_2_id"
+            ]
+        },
+        {
+            "team_id": "team_id_4",
+            "choices": [
+                "proj_3_id", "proj_1_id", "proj_2_id"
+            ]
+        }
+    ]
+}
+"""
--- services/views.py
+++ services/views.py
@@ -1,3 +1,5 @@
+import json
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,3 +8,47 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Hello!')
 
+
+""" Example Data:
+{
+    "projects": {
+        "proj_1_id": {"max_group_num": 3}, 
+        "proj_2_id": { "max_group_num": 3}
+    },
+    "wishes": [
+        {
+            "team_id": "team_id_1",
+            "choices": [
+                 "proj_1_id", "proj_2_id"
+            ]
+        },
+        {
+            "team_id": "team_id_2",
+            "choices": [
+                "proj_1_id", "proj_2_id"
+            ]
+        }
+    ]
+}
+"""
+
+def grouping(request):
+    data = json.loads(request.body)
+    print(data)
+    projects = data['projects']
+    wishes = data['wishes']
+    for wish in wishes:
+        for choice in wish['choices']:
+            choice = str(choice)
+            teams = projects[choice].get('teams', list())
+            if len(teams) < projects[choice]['max_group_num']:
+                teams.append(wish['team_id'])
+                projects[choice]['teams'] = teams
+                break
+    
+    teams = dict()
+    for project_id in projects.keys():
+        for team in projects[project_id].get('teams', list()):
+            teams[team] = project_id
+    
+    return JsonResponse(teams)
