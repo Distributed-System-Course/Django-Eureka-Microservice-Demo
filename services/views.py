7e31d1d91a1b7a54fe7ae83d7c48b57486facc85
henry_23 <henry.maoht@outlook.com>
2022-01-05 23:20:08 +0800

    fix: change data exchange format for 'grouping'

--- services/views.py
+++ services/views.py
@@ -11,44 +11,50 @@ def index(request):
 
 """ Example Data:
 {
-    "projects": {
-        "proj_1_id": {"max_group_num": 3}, 
-        "proj_2_id": { "max_group_num": 3}
-    },
+    "projects": [
+        {"id": "proj_1_id", "max_group_num": 1},
+        {"id": "proj_2_id", "max_group_num": 1} 
+    ],
     "wishes": [
         {
             "team_id": "team_id_1",
-            "choices": [
-                 "proj_1_id", "proj_2_id"
-            ]
+            "choices": [ "proj_1_id", "proj_2_id" ]
         },
         {
             "team_id": "team_id_2",
-            "choices": [
-                "proj_1_id", "proj_2_id"
-            ]
+            "choices": [ "proj_1_id", "proj_2_id" ]
         }
     ]
 }
 """
 
 def grouping(request):
-    data = json.loads(request.body)
-    print(data)
-    projects = data['projects']
-    wishes = data['wishes']
+    data = dict()
+    try:
+        data = json.loads(request.body)
+        # print('[Received]', data)
+        max_group_num = {
+            obj['id']: obj['max_group_num']
+            for obj in data['projects']
+        }
+        wishes = data['wishes']
+    except:
+        return JsonResponse(dict())
+    
+    teams_of_proj = dict()
+
     for wish in wishes:
         for choice in wish['choices']:
-            choice = str(choice)
-            teams = projects[choice].get('teams', list())
-            if len(teams) < projects[choice]['max_group_num']:
+            teams = teams_of_proj.get(choice, list())
+            teams_of_proj[choice] = teams
+            if len(teams) < max_group_num[choice]:
                 teams.append(wish['team_id'])
-                projects[choice]['teams'] = teams
+                # first fit; if fit, then break
                 break
     
-    teams = dict()
-    for project_id in projects.keys():
-        for team in projects[project_id].get('teams', list()):
-            teams[team] = project_id
+    the_proj_of_team = dict()
+    for project_id in teams_of_proj.keys():
+        for team in teams_of_proj.get(project_id, list()):
+            the_proj_of_team[team] = project_id
     
-    return JsonResponse(teams)
+    return JsonResponse(the_proj_of_team)
